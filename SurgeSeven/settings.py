109a8f8f074e7ee23d7bbc159b4cc4371add3f6d
Burger-karl <charleskalu863@gmail.com>
2024-10-16 20:46:11 +0100

    final commit

--- SurgeSeven/settings.py
+++ SurgeSeven/settings.py
@@ -168,17 +168,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
 
-
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+LOGIN_REDIRECT_URL = '/profile/'
 
 AUTH_USER_MODEL = 'users.User'
-
-LOGIN_REDIRECT_URL = 'profile/'
 
 
 AUTHENTICATION_BACKENDS = [
